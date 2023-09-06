2eb87926bdc443a8d99bfc13feafd023d5bbcb45
Laxminarayan Sharma <laxminarayannsharma@gmail.com>
2023-09-07 01:54:47 +0530

    BeanFactory: Added Spring Suppoer and example of beanfactory

--- SpringDemo/src/main/java/org/example/App.java
+++ SpringDemo/src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        Alien alien = (Alien) factory.getBean("alian");
+        alien.code();
         System.out.println( "Hello World!" );
     }
 }
